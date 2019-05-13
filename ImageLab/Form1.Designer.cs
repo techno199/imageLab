e8d8d61ee67d2e359e32fb60f666da18da46250f
ksan199 <techno199@live.ru>
2019-05-13 20:02:07 +0300

    Add project files.

---
+++ ImageLab/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ImageLab
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.openBtn = new System.Windows.Forms.Button();
+      this.Btn = new System.Windows.Forms.Button();
+      this.saveBtn = new System.Windows.Forms.Button();
+      this.pictureBox = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // openBtn
+      // 
+      this.openBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.openBtn.Location = new System.Drawing.Point(12, 333);
+      this.openBtn.Name = "openBtn";
+      this.openBtn.Size = new System.Drawing.Size(75, 23);
+      this.openBtn.TabIndex = 0;
+      this.openBtn.Text = "Открыть";
+      this.openBtn.UseVisualStyleBackColor = true;
+      this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+      // 
+      // Btn
+      // 
+      this.Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.Btn.Location = new System.Drawing.Point(93, 333);
+      this.Btn.Name = "Btn";
+      this.Btn.Size = new System.Drawing.Size(119, 23);
+      this.Btn.TabIndex = 1;
+      this.Btn.Text = "Подменить цвет";
+      this.Btn.UseVisualStyleBackColor = true;
+      this.Btn.Click += new System.EventHandler(this.shiftColorBtn_Click);
+      // 
+      // saveBtn
+      // 
+      this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.saveBtn.Location = new System.Drawing.Point(454, 333);
+      this.saveBtn.Name = "saveBtn";
+      this.saveBtn.Size = new System.Drawing.Size(99, 23);
+      this.saveBtn.TabIndex = 2;
+      this.saveBtn.Text = "Сохранить";
+      this.saveBtn.UseVisualStyleBackColor = true;
+      this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+      // 
+      // pictureBox
+      // 
+      this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox.Location = new System.Drawing.Point(13, 13);
+      this.pictureBox.Name = "pictureBox";
+      this.pictureBox.Size = new System.Drawing.Size(540, 314);
+      this.pictureBox.TabIndex = 3;
+      this.pictureBox.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(565, 368);
+      this.Controls.Add(this.pictureBox);
+      this.Controls.Add(this.saveBtn);
+      this.Controls.Add(this.Btn);
+      this.Controls.Add(this.openBtn);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button openBtn;
+    private System.Windows.Forms.Button Btn;
+    private System.Windows.Forms.Button saveBtn;
+    private System.Windows.Forms.PictureBox pictureBox;
+  }
+}
+
---
+++ ImageLab/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageLab
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    public Bitmap bmp { get; set; }
+    public Graphics graphics { get; set; }
+
+    private void openBtn_Click(object sender, EventArgs e)
+    {
+      var dialog = new OpenFileDialog();
+      dialog.Filter = "Image Files (*.BMP, *.JPG" +
+        "*.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
+      
+      if (dialog.ShowDialog() == DialogResult.OK)
+      {
+        var image = Image.FromFile(dialog.FileName);
+        bmp = new Bitmap(
+          image,
+          image.Width,
+          image.Height
+        );
+
+        pictureBox.Image = bmp;
+
+        graphics = Graphics.FromImage(pictureBox.Image);
+      }
+    }
+
+    private void saveBtn_Click(object sender, EventArgs e)
+    {
+      if (bmp == null) return;
+
+      var saveFileDialog = new SaveFileDialog();
+      saveFileDialog.Title = "Сохранить каринку как...";
+      saveFileDialog.OverwritePrompt = true;
+      saveFileDialog.CheckPathExists = true;
+      saveFileDialog.Filter =
+        "Bitmap File(*.bmp)|*.bmp|" +
+        "GIF File(*.gif)|*.gif|" +
+        "JPEG File(*.jpg)|*.jpg|" +
+        "PNG File(*.png)|*.png";
+      
+      if (saveFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        var fileName = saveFileDialog.FileName;
+        var fileExt = fileName.Remove(0, fileName.Length - 3);
+
+        switch (fileExt)
+        {
+          case "bmp":
+            bmp.Save(fileName, ImageFormat.Bmp);
+            break;
+          case "jpg":
+            bmp.Save(fileName, ImageFormat.Jpeg);
+            break;
+          case "gif":
+            bmp.Save(fileName, ImageFormat.Gif);
+            break;
+          case "png":
+            bmp.Save(fileName, ImageFormat.Png);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    private void shiftColorBtn_Click(object sender, EventArgs e)
+    {
+      if (bmp == null) return;
+
+      for (var i = 0; i < bmp.Width; i++)
+        for (var j = 0; j < bmp.Height; j++)
+        {
+          var pixel = bmp.GetPixel(i, j);
+          var givenColor = Color.FromArgb(pixel.R, pixel.G, pixel.B);
+          var hue = givenColor.GetHue();
+          var saturation = givenColor.GetSaturation();
+          var brightness = givenColor.GetBrightness();
+
+
+          if (
+            hue > 200 && 
+            hue < 240 &&
+            saturation > 0.4 &&
+            brightness > 0.2
+          )
+          {
+            var redColor = Color.FromArgb(255, 0, 0);
+            bmp.SetPixel(i, j, redColor);
+          }
+        }
+
+      Refresh();
+    }
+  }
+}
